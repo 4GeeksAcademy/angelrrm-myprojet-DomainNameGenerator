eb554acca2dd4660739e0a6e66fc1f2e924713f5
angelrrm1 <angelrramosm@icloud.com>
2025-01-15 11:57:37 +0000

    Modifique html y añadi funcion js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "our", "is"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extension = [".es", ".ve", ".eu"];
+
+let container = document.querySelector(".yellow-banner");
+let domainList = "";
+
+for (let p = 0; p < pronoun.length; p++) {
+  for (let a = 0; a < adj.length; a++) {
+    for (let n = 0; n < noun.length; n++) {
+      for (let e = 0; e < extension.length; e++) {
+        let domainName = pronoun[p] + adj[a] + noun[n] + extension[e];
+        domainList += domainName + "<br>";
+      }
+    }
+  }
+}
+
+container.innerHTML = domainList;
